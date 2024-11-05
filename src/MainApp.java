d86b86b7c365d6c9ffb1519ea483380acbfc2a29
Kirill Bolestev <kukushka_kukushkina_2012@mail.ru>
2024-11-05 16:48:21 +0300

    final project for 11/05/2024

--- src/MainApp.java
+++ src/MainApp.java
@@ -3,20 +3,29 @@ import java.nio.charset.StandardCharsets;
 
 public class MainApp {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         String s = "C:\\Users\\natal\\Downloads\\com.javarush.bolestev.cryptoanalyzer\\src\\Notes\\";
+        int keyFromUser = Validator.getKeyFromUser("4");
 
-        //Логика для выбора режима работы, вызов соответствующих методов
-        //FileManager.writeFile(Cipher.encrypt(FileManager.readFile(s + "Hamlet.txt", StandardCharsets.UTF_8), 5),
-        //    s + "CipHam.txt", false);
-        // FileManager.writeFile(Cipher.decrypt(FileManager.readFile(s + "CipHam.txt", StandardCharsets.UTF_8), 2),
-        //        s + "DeCipHam.txt", false);
-        // FileManager.writeFile(BruteForce.decryptByBruteForce(FileManager.readFile(s + "CipHam.txt", StandardCharsets.UTF_8), ALPHABET),
-        //           "DeCipHam.txt", true);
+        // Шифрование
+        FileManager.writeFile(Cipher.encrypt(FileManager.readFile(s + "Hamlet.txt", StandardCharsets.UTF_8), keyFromUser),
+                s + "CipHam.txt", false);
+
+        //Определяем наиболее вероятный ключ
         StatisticalAnalyzer statisticalAnalyzer = new StatisticalAnalyzer();
         int key = statisticalAnalyzer.findMostLikelyShift(FileManager.readFile(s + "CipHam.txt", StandardCharsets.UTF_8),
                 Alphabet.CYRILLIC.getAlphabet(), "о, е, а, и, н, т, с, р, в");
         System.out.println(key);
+
+        //Расшифровка
+        FileManager.writeFile(Cipher.decrypt(FileManager.readFile(s + "CipHam.txt", StandardCharsets.UTF_8), key),
+                s + "DeCipHam.txt", false);
+
+        //Грубая расшифровка. Записываем все варианты в один файл
+        FileManager.writeFile(BruteForce.decryptByBruteForce(FileManager.readFile(s + "CipHamBruteForce.txt",
+                        StandardCharsets.UTF_8), Alphabet.CYRILLIC.getAlphabet()),
+                "DeCipHam.txt", true);
+
     }
 }
--- src/Validator.java
+++ src/Validator.java
@@ -3,16 +3,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Validator {
-    public boolean isValidKey(int key, char[] alphabet) {
-        // Проверка ключа
-        return false;
-    }
+
     // Вспомогательный метод для проверки пути к файлу
     protected static void validateFilePath(String filePath) {
         if (filePath == null || filePath.trim().isEmpty()) {
             throw new IllegalArgumentException("Путь к файлу не может быть null или пустым.");
         }
     }
+
     // Вспомогательный метод для проверки существования файла
     protected static void checkFileExists(Path path) throws IOException {
         if (!Files.exists(path)) {
@@ -21,5 +19,22 @@ public class Validator {
         if (!Files.isRegularFile(path)) {
             throw new IOException("Указанный путь не является файлом: " + path);
         }
+    }
+
+    //Проверка что ключь число
+    protected static int getKeyFromUser(String sKey) {
+        int key = 0;
+        boolean validInput = false;
+
+        while (!validInput) {
+            try {
+                key = Integer.parseInt(sKey); // Пробуем преобразовать введенное значение в число
+                validInput = true; // Если преобразование успешно, выходим из цикла
+            } catch (NumberFormatException e) {
+                System.out.println("Ошибка: Ключ должен быть числом."); // Обрабатываем неверный ввод
+            }
+        }
+        return key; // Возвращаем
+
     }
 }
